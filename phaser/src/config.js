7259ce45ae0934b42ff8714889d2fa735bff4b3d
NSRB@steam <nasyarobby@gmail.com>
2025-03-06 18:00:46 +0700

    Fix for mobile mode

--- phaser/src/config.js
+++ phaser/src/config.js
@@ -1,6 +1,7 @@
 const IS_PORTRAIT = window.innerWidth < window.innerHeight
+const ratio = window.innerWidth / window.innerHeight
 const HEIGHT =!IS_PORTRAIT ? window.innerHeight : 1280
-const WIDTH = !IS_PORTRAIT ? window.innerWidth : 800
+const WIDTH = !IS_PORTRAIT ? window.innerWidth : HEIGHT * ratio
 const HAND_HEIGHT = 200
 
 export {WIDTH, HEIGHT, HAND_HEIGHT}
